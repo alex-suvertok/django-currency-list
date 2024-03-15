5dc44a1e812f07ee5d550236bf9869bda8be6cb4
alex.suvertok <alex.suvertok@regulus.team>
2024-03-15 21:08:56 +0200

    Change version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django_currency_list',
-    version='0.1.4',
+    version='0.1.5',
     packages=find_packages(),
     license='MIT',
     author='Alex Suvertok',
