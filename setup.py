c9d4db215680cb8c0cbd07e101b3f775906c6155
alex.suvertok <alex.suvertok@regulus.team>
2024-06-20 10:59:02 +0300

    Update init

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-currency-list',
-    version='0.1.4',
+    version='0.1.5',
     packages=find_packages(),
     license='MIT',
     author='Alex Suvertok',
